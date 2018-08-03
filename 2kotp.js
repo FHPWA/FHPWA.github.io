e79691acc20811c896103c5abdab2b44e0d96082
Kieran <alarmcuddly@outlook.com>
2018-08-03 11:47:09 +0100

    20180802
    index.html is now the homepage. WhiteCat released. further modifications to navbar and forms

---
+++ 2kotp.js
@@ -0,0 +1,29 @@
+function start() {
+    var charint, choice, inputstring, keystring, keystring2, outstring, inputlength;
+    keystring = document.getElementById("fieldkey").value.replace(/\s/g,'');
+    keystring2 = document.getElementById("fieldkey2").value.replace(/\s/g,'');
+	choice = document.getElementById("rad").checked;
+	
+    outstring = "";
+	inputstring = document.getElementById("input").value;
+    //inputlength = inputstring.length;
+    inputlength = inputstring.normalize().length;
+    for (var index = 0; (index < inputlength); index += 1) {
+        if ((index >= keystring.length)) {
+            keystring += keystring;
+        }
+        if ((index >= keystring2.length)) {
+            keystring2 += keystring2;
+        }
+        if ((choice === true)) {
+            charint = inputstring.charCodeAt(index) + keystring.charCodeAt(index) + keystring2.charCodeAt(index) ;
+        } else {
+            charint = inputstring.charCodeAt(index) - keystring.charCodeAt(index) - keystring2.charCodeAt(index);
+        }
+        outstring += String.fromCharCode(charint);
+        //console.log("Input: " + inputstring[index]);
+        //console.log("Output: " + String.fromCharCode(charint));
+        
+    }
+	document.getElementById("output").value = outstring;
+}
