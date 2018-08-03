f85421a64881aaa5bccec3d61929c2b3aad5a352
Kieran <alarmcuddly@outlook.com>
2018-08-03 12:00:09 +0100

    20180803.4
    changed cache version

--- sw.js
+++ sw.js
@@ -3,12 +3,14 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE_ID = 'precache-v1';
+const PRECACHE_ID = 'precache-20180803';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const URLS = [
   'index.html',
+  'otp.html',
+  '2kotp.html',
   './', 
   'theme.css'
 ];
