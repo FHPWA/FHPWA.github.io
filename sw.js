dddf5d6143b8c708af25412a7f99e4b6f42b88b6
Kieran W <41634689+FredHappyface@users.noreply.github.com>
2018-12-15 20:24:17 +0000

    20181215

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE_ID = 'precache-v20181215.4';
+const PRECACHE_ID = 'precache-v20181215.5';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
