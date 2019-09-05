bd08d168cbb088e3732a0ddffe352ecae9e8e162
Kieran W <41634689+FredHappyface@users.noreply.github.com>
2019-09-05 21:23:34 +0100

    Radio for selected setting remains active

--- scripts/settings.js
+++ scripts/settings.js
@@ -4,6 +4,14 @@ let textRadios = document.getElementsByName("textR");
 let themeList = ["", "light", "dark", "black", "auto"];
 let textList = ["", "0.75", "1", "1.5"];
 
+let textIds = ["", "small", "med", "large"]
+
+document.getElementById(data.theme).checked = true;
+textRadioIndex = textList.indexOf(data.text);
+if (textRadioIndex > -1){
+	document.getElementById(textIds[textRadioIndex]).checked = true;
+}
+
 function saveTheme() {
 	let theme = 0;
 
