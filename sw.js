20c3c16c11536d808e52314534340a772829db1c
Kieran W <41634689+FredHappyface@users.noreply.github.com>
2019-11-14 20:45:53 +0000

    removed legacy icons

--- sw.js
+++ sw.js
@@ -1,9 +1,7 @@
-﻿const cacheVersion = "fredhappyface.github.io-2019.11.4";
+﻿const cacheVersion = "fredhappyface.github.io-2019.11.5";
 const urlsToPrefetch = [
 	"/",
-	"/PWA.Home/",
 ];
-
 
 self.addEventListener("install", function (event) {
 	event.waitUntil(
@@ -13,11 +11,11 @@ self.addEventListener("install", function (event) {
 	);
 });
 
-self.addEventListener("activate", function(event) {
+self.addEventListener("activate", function (event) {
 	event.waitUntil(
-		caches.keys().then(function(keyList){
-			return Promise.all(keyList.map(function(key){
-				if (key !== cacheVersion){
+		caches.keys().then(function (keyList) {
+			return Promise.all(keyList.map(function (key) {
+				if (key !== cacheVersion) {
 					return caches.delete(key);
 				}
 			}));
@@ -26,10 +24,26 @@ self.addEventListener("activate", function(event) {
 	return self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
-  let responsePromise = caches.match(event.request).then((response) => {
-    return response || fetch(event.request);
-  });
-
-  event.respondWith(responsePromise);
+self.addEventListener('fetch', function (event) {
+	event.respondWith(fromNetwork(event.request, 400).catch(function () {
+		return fromCache(event.request);
+	}));
 });
+
+function fromNetwork(request, timeout) {
+	return new Promise(function (fulfill, reject) {
+		var timeoutId = setTimeout(reject, timeout);
+		fetch(request).then(function (response) {
+			clearTimeout(timeoutId);
+			fulfill(response);
+		}, reject);
+	});
+}
+
+function fromCache(request) {
+	return caches.open(cacheVersion).then(function (cache) {
+		return cache.match(request).then(function (matching) {
+			return matching || Promise.reject('no-match');
+		});
+	});
+}
